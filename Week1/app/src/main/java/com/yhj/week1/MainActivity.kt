4e2ff99f101cdf6f4e66158ce06d0beb2e1051a9
yonghanJu <ju990828@naver.com>
2023-10-28 17:30:33 +0900

    [feat] #1 마이페이지(MainActivity) 구현

--- Week1/app/src/main/java/com/yhj/week1/MainActivity.kt
+++ Week1/app/src/main/java/com/yhj/week1/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.yhj.week1
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.yhj.week1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding: ActivityMainBinding
 
     override
@@ -13,6 +15,8 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         binding.lifecycleOwner = this
+        binding.nameRepo = ServiceLocator.getService<NameRepository>()
+        binding.action = { startActivity(Intent(this@MainActivity, EditActivity::class.java)) }
         setContentView(binding.root)
     }
 }
