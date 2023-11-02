8aae5462f310428d43d50dacaef21491dfab6aa4
yonghanJu <ju990828@naver.com>
2023-11-02 23:15:11 +0900

    [feat] #3 바텀 네비게이션 구현

--- Week2/app/src/main/java/com/yhj/week2/MainActivity.kt
+++ Week2/app/src/main/java/com/yhj/week2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.yhj.week2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.yhj.week2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initViews()
+    }
+
+    private fun initViews() = with(binding) {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragment_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        bottomNav.setupWithNavController(navController)
     }
 }
