0cc658d2f1c248b4e4ca1ead812619e6c8e3a501
yonghanJu <ju990828@naver.com>
2023-11-08 15:45:47 +0900

    [refactor] #5 UserRepository Context 의존성 분리

--- app/src/main/java/com/yhj/gdscandroidstudy/di/appModule.kt
+++ app/src/main/java/com/yhj/gdscandroidstudy/di/appModule.kt
@@ -1,6 +1,7 @@
 package com.yhj.gdscandroidstudy.di // ktlint-disable filename
 
 import com.yhj.gdscandroidstudy.data.UserRepository
+import com.yhj.gdscandroidstudy.data.UserRepositoryImpl
 import com.yhj.gdscandroidstudy.ui.edit.EditViewModel
 import com.yhj.gdscandroidstudy.ui.home.HomeViewModel
 import com.yhj.gdscandroidstudy.ui.mypage.MyPageViewModel
@@ -8,7 +9,7 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    single { UserRepository(get()) }
+    single<UserRepository> { UserRepositoryImpl(get()) }
     viewModel { EditViewModel(get()) }
     viewModel { MyPageViewModel(get()) }
     viewModel { HomeViewModel() }
