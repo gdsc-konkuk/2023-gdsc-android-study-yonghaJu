e2670923b02a1d9096d6944b19c086048a40186f
yonghanJu <ju990828@naver.com>
2023-11-08 17:14:44 +0900

    [refactor] #5 더미 리스트 -> TodoRepository 마이그레이션

--- app/src/main/java/com/yhj/gdscandroidstudy/di/appModule.kt
+++ app/src/main/java/com/yhj/gdscandroidstudy/di/appModule.kt
@@ -1,7 +1,14 @@
 package com.yhj.gdscandroidstudy.di // ktlint-disable filename
 
+import androidx.room.Room
+import com.yhj.gdscandroidstudy.data.AppDatabase
+import com.yhj.gdscandroidstudy.data.AppDatabase.Companion.DATABASE_NAME
+import com.yhj.gdscandroidstudy.data.todo.TodoDataSource
+import com.yhj.gdscandroidstudy.data.todo.TodoLocalDataSource
+import com.yhj.gdscandroidstudy.data.todo.TodoRepositoryImpl
 import com.yhj.gdscandroidstudy.data.user.UserRepository
 import com.yhj.gdscandroidstudy.data.user.UserRepositoryImpl
+import com.yhj.gdscandroidstudy.domain.TodoRepository
 import com.yhj.gdscandroidstudy.ui.edit.EditViewModel
 import com.yhj.gdscandroidstudy.ui.home.HomeViewModel
 import com.yhj.gdscandroidstudy.ui.mypage.MyPageViewModel
@@ -12,5 +19,15 @@ val appModule = module {
     single<UserRepository> { UserRepositoryImpl(get()) }
     viewModel { EditViewModel(get()) }
     viewModel { MyPageViewModel(get()) }
-    viewModel { HomeViewModel() }
+    viewModel { HomeViewModel(get()) }
+    single {
+        Room.databaseBuilder(
+            get(),
+            AppDatabase::class.java,
+            DATABASE_NAME,
+        ).build()
+    }
+    single { get<AppDatabase>().todoDao() }
+    single<TodoRepository> { TodoRepositoryImpl(get()) }
+    single<TodoDataSource> { TodoLocalDataSource(get()) }
 }
