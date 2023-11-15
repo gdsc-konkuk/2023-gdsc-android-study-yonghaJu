a5883e3e57c089a1e8dc81a8dc7222fa62a78b1b
yonghanJu <ju990828@naver.com>
2023-11-16 01:32:30 +0900

    [feat] #7 사진 관련 레포 DI 모듈 구현

--- app/src/main/java/com/yhj/gdscandroidstudy/di/appModule.kt
+++ app/src/main/java/com/yhj/gdscandroidstudy/di/appModule.kt
@@ -1,23 +1,34 @@
 package com.yhj.gdscandroidstudy.di // ktlint-disable filename
 
 import androidx.room.Room
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.yhj.gdscandroidstudy.data.AppDatabase
 import com.yhj.gdscandroidstudy.data.AppDatabase.Companion.DATABASE_NAME
+import com.yhj.gdscandroidstudy.data.photo.PhotoRepositoryImpl
+import com.yhj.gdscandroidstudy.data.photo.PhotoService
+import com.yhj.gdscandroidstudy.data.photo.UnSplashInterceptor
 import com.yhj.gdscandroidstudy.data.todo.TodoDataSource
 import com.yhj.gdscandroidstudy.data.todo.TodoLocalDataSource
 import com.yhj.gdscandroidstudy.data.todo.TodoRepositoryImpl
 import com.yhj.gdscandroidstudy.data.user.UserRepositoryImpl
+import com.yhj.gdscandroidstudy.domain.PhotoRepository
+import com.yhj.gdscandroidstudy.domain.SetRandomPhotoUseCase
 import com.yhj.gdscandroidstudy.domain.TodoRepository
 import com.yhj.gdscandroidstudy.domain.UserRepository
 import com.yhj.gdscandroidstudy.ui.edit.EditViewModel
 import com.yhj.gdscandroidstudy.ui.home.HomeViewModel
 import com.yhj.gdscandroidstudy.ui.mypage.MyPageViewModel
+import com.yhj.gdscandroidstudy.util.NETWORK
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import retrofit2.Retrofit
 
 val appModule = module {
     single<UserRepository> { UserRepositoryImpl(get()) }
-    viewModel { EditViewModel(get()) }
+    viewModel { EditViewModel(get(), get()) }
     viewModel { MyPageViewModel(get(), get()) }
     viewModel { HomeViewModel(get()) }
     single {
@@ -30,4 +41,18 @@ val appModule = module {
     single { get<AppDatabase>().todoDao() }
     single<TodoRepository> { TodoRepositoryImpl(get()) }
     single<TodoDataSource> { TodoLocalDataSource(get()) }
+    single<PhotoRepository> { PhotoRepositoryImpl(get()) }
+    single {
+        OkHttpClient.Builder().addInterceptor(UnSplashInterceptor()).build()
+    }
+    single {
+        val json = Json {
+            ignoreUnknownKeys = true
+        }
+        Retrofit.Builder().baseUrl(NETWORK.UNSPLASH.BASE_URL).client(get()).addConverterFactory(
+            json.asConverterFactory(NETWORK.UNSPLASH.MEDIA_TYPE.toMediaType()),
+        ).build()
+    }
+    single { SetRandomPhotoUseCase(get(), get()) }
+    single { get<Retrofit>().create(PhotoService::class.java) }
 }
