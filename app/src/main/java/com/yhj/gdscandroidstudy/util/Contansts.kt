18bac7293a67104433eb487831f32efca2792793
yonghanJu <ju990828@naver.com>
2023-11-16 01:28:15 +0900

    [faet] #7 네트워크 관련 상수 추가

--- app/src/main/java/com/yhj/gdscandroidstudy/util/Contansts.kt
+++ app/src/main/java/com/yhj/gdscandroidstudy/util/Contansts.kt
@@ -1,3 +1,17 @@
 package com.yhj.gdscandroidstudy.util
 
 val SUBSCRIPTION_TIMEOUT: Long get() = 5000L
+
+object NETWORK {
+    object UNSPLASH {
+        const val BASE_URL = "https://api.unsplash.com"
+
+        const val MEDIA_TYPE = "application/json"
+
+        const val CLIENT_ID = "client_id"
+
+        object API {
+            const val GET_RANDOM = "/photos/random"
+        }
+    }
+}
